3726fd431666992e3684e17b29fb2f1ba077f852
Abhishek Yadav <yabhishekk480@gmail.com>
2025-09-19 07:32:03 +0530

    Portfolio website UI

---
+++ PersonalWeb/README.md
@@ -0,0 +1,5 @@
+ A random portfolio website to learn & practice  routing in React.js .
+  Learned about Link and Navlink working  
+  Learned to make several components of the UI and using them on the page 
+  ![screenshot ](./portfolio1.jpg) 
+  ![screenshot](./portfolio2.jpg)
---
+++ PersonalWeb/src/main.jsx
@@ -0,0 +1,46 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { createBrowserRouter,RouterProvider } from 'react-router-dom'
+import Layout from './components/Layout'
+import Home from './components/Home/Home.jsx'
+import About from './components/About/About.jsx'
+  import Contact from './components/Contact/Contact.jsx' 
+  import Github ,{githubInfoLoader} from './components/Github/Github.jsx' 
+  import User from'./components/User/User.jsx'
+const router =createBrowserRouter([
+  { 
+    path:'/', 
+    element:<Layout/>,
+    children :[
+      { path:'/', 
+        element: < Home/>
+      },
+
+      { path:"about",
+        element: < About/>
+      },
+      { 
+        path:"contact",
+        element:< Contact/>
+      } ,
+      { 
+       
+        loader:githubInfoLoader,
+        path:"Github",
+        element:<Github/> ,
+      },
+      {
+        path:"User/:userid",
+        element:<User/>
+      }
+    ]
+  }
+])
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+  <RouterProvider router={router}/>
+  </StrictMode>,
+)
